42c15c9ff981a6a6944760c7461b5baed7e4e008
Thiago Maia <thiago.maia971@gmail.com>
2017-03-07 22:18:15 -0300

    Gerando Trx e o HTML

---
+++ GerarTestes.App/GerarTest.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace GerarTestes.App
+{
+    partial class GerarTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(99, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Source DLL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ButtonFileDllTest);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Generate TRX File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ButtonGerarTRX);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(150, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Generate HTML Test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ButtonGerarHTML);
+            // 
+            // GerarTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 113);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "GerarTest";
+            this.Text = "GerarTest";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ GerarTestes.App/GerarTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerarTestes.App
+{
+    public partial class GerarTest : Form
+    {
+
+        public string SourceDll { get; set; }
+        public string MyProperty { get; set; }
+        public GerarTest()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void ButtonFileDllTest(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var fullName = openFileDialog1.FileName;
+
+                MessageBox.Show(fullName);
+            }
+        }
+
+        private void ButtonGerarTRX(object sender, EventArgs e)
+        {
+            var pathCSPROJ = openFileDialog1.FileName;
+            var fileResultTest = GetFileResultTest();
+            var exe = "./MSTest.exe";
+            var mensagemRetorno = $"The file {pathCSPROJ}\\ResultTest.trx was created.";
+
+            var comando = string.Format("/testcontainer:{0} /resultsfile:{1} ", pathCSPROJ, fileResultTest);
+
+            ExecutarCommandLine(exe, comando, mensagemRetorno);
+
+        }
+
+        private void ButtonGerarHTML(object sender, EventArgs e)
+        {
+            var pathCSPROJ = GetPathCSPROJ();
+            var fileResultTest = GetFileResultTest();
+            var exe = "./specflow.exe";
+            var mensagemRetorno = $"The file HTML is created in the same folder of the ResultTest.trx";
+
+            var comando = string.Format("mstestexecutionreport {0} /testResult:{1}", pathCSPROJ, fileResultTest);
+
+            ExecutarCommandLine(exe, comando, mensagemRetorno);
+            fileResultTest = fileResultTest.Replace("trx", "html");
+
+            File.Move(Path.GetFullPath("./") + "\\TestResult.html", fileResultTest);
+        }
+
+        private static void ExecutarCommandLine(string exe, string comando, string mensagemRetorno)
+        {
+            using (Process processo = new Process())
+            {
+                processo.StartInfo.FileName = exe;
+
+                processo.StartInfo.Arguments = comando;
+
+                processo.StartInfo.RedirectStandardOutput = true;
+                processo.StartInfo.UseShellExecute = false;
+                processo.StartInfo.CreateNoWindow = true;
+
+                processo.Start();
+                processo.WaitForExit();
+
+                string saida = processo.StandardOutput.ReadToEnd();
+
+                MessageBox.Show(mensagemRetorno);
+            }
+        }
+
+        private string GetFileResultTest()
+        {
+            var pathCSPROJ = GetPathCSPROJ().Split('\\').ToList();
+            pathCSPROJ.RemoveRange(pathCSPROJ.Count - 2, 2);
+
+            var pathResultTest = string.Join("\\", pathCSPROJ);
+
+            pathResultTest += "\\TestResults\\ResultTest.trx";
+
+            return pathResultTest;
+        }
+
+        private string GetPathCSPROJ()
+        {
+            var path = openFileDialog1.FileName.Split(new string[] { "bin" }, StringSplitOptions.RemoveEmptyEntries).FirstOrDefault();
+            var b = path.Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries).LastOrDefault();
+            path += b + ".csproj";
+
+            return path;
+        }
+    }
+}
